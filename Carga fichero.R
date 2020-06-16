bac6099a387a68ce50a2465ff2a9859f63e4500c
Amaia <amaia.alcalde@alumni.mondragon.edu>
2020-06-16 10:48:30 +0200

    Procesamiento sociedades limitadas

--- Carga fichero.R
+++ Carga fichero.R
@@ -8,3 +8,25 @@ datos<-muestra04
 datos[nchar((datos$NifCif))==0,][,1]<-NA
 datos <- datos[!is.na(datos$NifCif),]
 datos<- datos[!(datos$NifCif==0),]
+
+# PROCESAMIENTO SOCIEDADES LIMITADAS --------------------------------------
+library(dplyr)
+regexb <- "(^B)"
+a <- as.vector(grepl(pattern = regexb, x = datos$NifCif))
+datos_lim <- datos %>%
+  mutate(nif_imp = a)
+
+datos_lim <- datos_lim %>%
+  filter(datos_lim$nif_imp == "TRUE")
+
+datos_lim<-datos_lim[(nchar((datos_lim$NifCif))==9),]
+
+regexa<-"$[:digit:]{8}"
+aa <- as.vector(grepl(pattern = regexa, x = datos_lim$NifCif))
+datos_lim <- datos %>%
+  mutate(nif_imp = aa)
+
+datos_lim <- datos_lim %>%
+  filter(datos_lim$nif_imp == "TRUE")
+
+#NO HE CONSEGUIDO SACAR EL NÚMERO EXACTO, PERO SON MENOS DE 195
