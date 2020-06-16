b5853d5d4075546bf8d8dbb565f624ec47bd446e
Amaia <amaia.alcalde@alumni.mondragon.edu>
2020-06-16 09:29:28 +0200

    Asignacion nombre de columnas

--- Carga fichero.R
+++ Carga fichero.R
@@ -1,2 +1,3 @@
 load("C:/Users/amaia/OneDrive/Escritorio/1 BDATA/DATA SCIENCE/Recu_DS/Recu_examen_ds/muestra04.RData")
 #la muestra contiene 332086 instancias (filas) y 3 variables (columnas)
+colnames(muestra04)<-c("NifCif", "nombre", "genero")
